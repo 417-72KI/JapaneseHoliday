32f86604f93febd426fc5c97ea9f3b9e8d3a9436
417-72KI <417.72ki@gmail.com>
2024-11-06 16:34:00 +0900

    fix test

--- Tests/JapaneseHolidayTests/JapaneseHolidayTests.swift
+++ Tests/JapaneseHolidayTests/JapaneseHolidayTests.swift
@@ -3,18 +3,30 @@ import Testing
 @testable import JapaneseHoliday
 
 @Test(arguments: [
-    ("2024-01-01", true),
-    ("2024-01-02", false),
+    ("2024-11-03", 2024, 11, 3, "文化の日"),
+    ("2024-11-04", 2024, 11, 4, "休日"),
+    ("2024-11-23", 2024, 11, 23, "勤労感謝の日"),
 ])
-func holidays(_ ymd: String, _ expected: Bool) async throws {
-    #expect((holidays[ymd] != nil) == expected)
+func holiday(_ ymd: String, _ year: Int, _ month: Int, _ day: Int, _ name: String) async throws {
+    let holiday = try #require(Holidays[ymd])
+    #expect(holiday.year == year)
+    #expect(holiday.month == month)
+    #expect(holiday.day == day)
+    #expect(holiday.name == name)
+}
+
+@Test(arguments: [
+    "2024-01-02"
+])
+func notHoliday(_ ymd: String) async throws {
+    #expect(Holidays[ymd] == nil)
 }
 
 @Test(arguments: [
     (1704034800, true),  // 2024/1/1
     (1704121200, false), // 2024/1/2
 ])
-func holiday(_ timeInterval: TimeInterval, _ expected: Bool) async throws {
+func holidayOfDate(_ timeInterval: TimeInterval, _ expected: Bool) async throws {
     let holiday = JapaneseHoliday.holiday(ofDate: .init(timeIntervalSince1970: timeInterval))
     #expect((holiday != nil) == expected)
 }
